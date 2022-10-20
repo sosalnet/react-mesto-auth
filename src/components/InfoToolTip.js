a573909931d76d505a67e1f0781891316864a9fd
luciD <sosalnet@yandex.ru>
2022-10-20 23:07:37 +0300

    done 1.0

--- src/components/InfoToolTip.js
+++ src/components/InfoToolTip.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
 function InfoToolTip({ onClose, isOpen, resultMessage }) {
+    return(
     <div className={`popup popup__preview ${isOpen ? 'popup_opened' : ''}`}>
-        <div className='popup__content'>
+        <div className='popup__container'>
             <img className='popup__image-info' src={resultMessage.image} alt={resultMessage.image} />
             <h2 className='popup__title popup__title_message-info'>
                 {resultMessage.text}
             </h2>
-            <button className='' type='button' onClick={onClose}></button>
+            <button className='popup__close-button' type='button' onClick={onClose}></button>
         </div>
     </div>
+    )
 }
 
 export default InfoToolTip;
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,11 +1,11 @@
-export const BASE_URL = 'https://api.nomoreparties.co';
+export const BASE_URL = "https://auth.nomoreparties.co";
 
 const check = (res) => {
     if (res.ok) {
-      return res.json();
+        return res.json();
     }
     return Promise.reject(res.status);
-  };
+};
 
 export const register = (password, email) => {
     return fetch(`${BASE_URL}/signup`, {
@@ -13,26 +13,26 @@ export const register = (password, email) => {
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({password: password, email: email })
+        body: JSON.stringify({ password: password, email: email })
     }).then(check);
-}; 
+};
 
 export const authorize = (email, password) => {
     return fetch(`${BASE_URL}/signin`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({email, password})
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password })
     }).then(check);
-  }; 
-  
-  export const getContent = (token) => {
+};
+
+export const getContent = (token) => {
     return fetch(`${BASE_URL}/users/me`, {
         method: 'GET',
         headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${token}`
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`
         },
-      }).then(check);
-  }
+    }).then(check);
+}
